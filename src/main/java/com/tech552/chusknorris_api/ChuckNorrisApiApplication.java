0365aeb2614cbb1733d0944ef82fb691373761ee
KalilK13 <kalilkennedy13@gmail.com>
2024-06-07 00:21:39 -0400

    finished chuck norris api hw.

---
+++ src/main/java/com/tech552/chusknorris_api/ChuckNorrisApiApplication.java
@@ -0,0 +1,56 @@
+package com.tech552.chusknorris_api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableScheduling
+public class ChuckNorrisApiApplication {
+	private Joke joke = new Joke();
+
+	private static final Logger log = LoggerFactory.getLogger(ChuckNorrisApiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisApiApplication.class, args);
+	}
+
+	public Joke getJoke() {
+		return joke;
+	}
+
+	public void setJoke(Joke joke) {
+		this.joke = joke;
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	@Profile("!test")
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			while(true) {
+				setJoke(restTemplate.getForObject("https://api.chucknorris.io/jokes/random", Joke.class));
+			}
+		};
+	}
+	@Scheduled(fixedRate = 5000)
+	public void scheduledJoke(){
+		log.info(joke.toString());
+	}
+
+
+
+}
